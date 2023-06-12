8563de31472ad8f0d8bb8835a65c1c1b713aa2c8
Roberto Cárdenas <superjrcm@gmail.com>
2023-06-12 01:18:24 -0500

    Hotfix: removed bad route

--- src/components/common/footer/Footer.tsx
+++ src/components/common/footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <nav className="py-8 border-t border-secondary">
       <ul className="flex flex-row container justify-around max-w-lg">
-        <Link href="#home" className="hover:text-primary-light">Home</Link>
+        <Link href="/" className="hover:text-primary-light">Home</Link>
         <Link href="#about" className="hover:text-primary-light">About</Link>
         <Link href="#work" className="hover:text-primary-light">Work</Link>
       </ul>
