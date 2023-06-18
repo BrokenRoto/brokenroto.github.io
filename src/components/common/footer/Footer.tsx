7f7bec6eab19ba09eb60dc79cfe44f540daff056
Roberto Cárdenas <superjrcm@gmail.com>
2023-06-18 01:43:32 -0500

    Added footer note

--- src/components/common/footer/Footer.tsx
+++ src/components/common/footer/Footer.tsx
@@ -3,7 +3,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <nav className="py-8 border-t border-secondary">
+    <nav className="py-8 border-t border-secondary flex flex-col relative">
+      <p className="absolute -top-12 left-1/2 -translate-x-1/2 text-xs text-center text-primary-lighter">Powered by Next.js and Tailwind CSS</p>
       <ul className="flex flex-row container justify-around max-w-lg">
         <Link href="/" className="hover:text-primary-light">Home</Link>
         <Link href="#about" className="hover:text-primary-light">About</Link>
